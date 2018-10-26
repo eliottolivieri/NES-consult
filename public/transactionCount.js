f12e902164b36ea93b97e35d1dfa59510dac107d
eliottolivieri <eliott.94@hotmail.com>
2018-10-26 00:07:28 -0400

    int to string

--- public/transactionCount.js
+++ public/transactionCount.js
@@ -5,6 +5,9 @@ function donuts(){
     var myFirst = new Date(1998,5,10);
     var myNow = new Date();
     var diffz = myNow - myFirst;
-    var transactionCount = diffz * 3
+    var transactionCount = diffz * 3;
+    transactionCount.toLocaleString();
+
+
 $("#countEm").text(transactionCount);
 }
