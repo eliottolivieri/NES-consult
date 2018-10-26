967e029521aca0229cc9cff1403fdd08386f0c58
eliottolivieri <eliott.94@hotmail.com>
2018-10-25 23:43:27 -0400

    ad

--- public/transactionCount.js
+++ public/transactionCount.js
@@ -2,6 +2,8 @@
 setInterval(donuts,1000);
 
 function donuts(){
-
-$("#countEm").text("apple");
+    var myFirst = new Date(1998,5,10);
+    var myNow = new Date();
+    var diffz = myFirst - myNow;
+$("#countEm").text(diffz);
 }
