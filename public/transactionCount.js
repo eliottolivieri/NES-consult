8c7899eca906a9be54d371cf641e1e8c03e9b827
eliottolivieri <eliott.94@hotmail.com>
2018-10-25 23:18:03 -0400

    a

--- public/transactionCount.js
+++ public/transactionCount.js
@@ -1,9 +1,1 @@
-
-setInterval(
-    function transCount() {
-       // var myFirstTime = new Date(1998,5,10);
-       // var myCurrentTime = new Date.now(); 
-       // var diffz = (myCurrentTime - myFirstTime);
-
-        document.getElementById("countEm").text("aaapppppllleess");
-    },100);
+document.getElementById("countEm").text("apple");
