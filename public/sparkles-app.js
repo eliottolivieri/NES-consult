1e3e2bfcefa30d33f7eae713a0a4adeb3da229f4
Eliott <eliott.94@hotmail.com>
2018-11-07 22:37:51 -0500

    l

--- public/sparkles-app.js
+++ public/sparkles-app.js
@@ -1,5 +1,9 @@
 particlesJS.load('particles-js', 'particles.json', function() {
     //console.log('callback - particles.js config loaded');
     });
+
+particlesJS.load('particles-js2', 'particles.json', function() {
+    //console.log('callback - particles.js config loaded');
+    });
     
     
